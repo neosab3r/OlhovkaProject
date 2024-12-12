4c34d6ad35ef7dfad134b9fb3ab732005bdcb453
Denis Gabbasov <gdidenis@mail.ru>
2024-12-12 05:22:04 +0500

    rework first description

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./Components/Header";
-import Centre from "./Components/Centre";
+import HomePage from "./Components/Home/HomePage";
 import Contacts from "./Components/Contacts";
 import DoctorsPage from "./Components/DoctorsPage";
 import RehabilitationPage from "./Components/RehabilitationPage";
@@ -16,15 +16,14 @@ function App() {
             <div className="content">
                 <Routes>
                     {/* Маршруты для всех страниц */}
-                    <Route path="/" element={<Centre />} />
-                    <Route path="/centre" element={<Centre />} />
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/centre" element={<HomePage />} />
                     <Route path="/stacionar" element={<DayHospitalPage />} />
                     <Route path="/doctors" element={<DoctorsPage />} />
                     <Route path="/rehabilitation" element={<RehabilitationPage />} />
                     <Route path="/routes" element={<RoutesPage />} />
                 </Routes>
             </div>
-
             <Contacts /> {/* Контакты всегда отображаются */}
         </div>
     );
---
+++ src/Components/Home/HomePage.tsx
@@ -0,0 +1,26 @@
+﻿import "../../Styles/Home/HomePage.css";
+
+import React from "react";
+import {SliderSection} from "../SliderSection";
+import AboutComponent from "./AboutComponent";
+
+
+const HomePage: React.FC = () => {
+    const images = require.context('../../Images/HomeImages', true);
+    const imageList = images.keys().map(image => images(image));
+
+    return (
+        <div className="home-container">
+            <SliderSection images={imageList}></SliderSection>
+            {/*<div className="collage">
+                <img src={require('../Images/OlhovkaCollage.png')} alt="OlhovkaCollageAlt"/>
+            </div>*/}
+            <AboutComponent />
+        </div>
+    );
+};
+
+export default HomePage;
+
+
+
